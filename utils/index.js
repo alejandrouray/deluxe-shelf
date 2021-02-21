58ba09b5b9069309794e88c68c3cbdf82d17b2ac
Sanoja Hector (Producto y Tecnologia) <hsanoja@lanacion.com.ar>
2021-02-21 18:33:15 -0300

    Vercel deployment 2.0

--- utils/index.js
+++ utils/index.js
@@ -6,6 +6,9 @@ export const fetchAPI = async (url) => await fetch(process.env.ENDPOINT + url);
 export const jsonFetch = async (res) => await res.json();
 
 export const getAPIData = async (url) => {
-  console.log(await jsonFetch(await fetchAPI(url)));
-  return await jsonFetch(await fetchAPI(url));
+  try {
+    jsonFetch(await fetchAPI(url));
+  } catch (error) {
+    console.log(error);
+  }
 };
